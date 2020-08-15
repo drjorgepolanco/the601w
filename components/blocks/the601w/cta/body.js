314f780bf04e8d5cb5b441648cdf6609e5f126fc
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.com>
2020-08-15 01:44:35 -0500

    remove extra space from cta content

--- components/blocks/the601w/cta/body.js
+++ components/blocks/the601w/cta/body.js
@@ -1,5 +1,5 @@
 
-import { FmtBlock, LinkGroup, TextTitle } from '../../../ui';
+import { FmtBlock, LinkGroup } from '../../../ui';
 import classes from '../../../../styles/classes.module.scss';
 
 export const BlockBody = ({ block }) => {
@@ -9,8 +9,8 @@ export const BlockBody = ({ block }) => {
     <FmtBlock attr={ body } attrName={ 'body' }>
       <div className="body" style={{ textAlign: 'center' }}>
         <div className="ow-text ow-group">
-          <TextTitle attr={ body } style={{ textTransform: 'none' }}  />
-          { (body.text_body)  && <div className={ classes.text_body } dangerouslySetInnerHTML={{ __html: body.text_body }} /> }
+          { (body.text_title) && <h2 style={{ textTransform: 'none' }} dangerouslySetInnerHTML={{ __html: body.text_title }} /> }
+          { (body.text_body)  && <div className={ `${classes.text_body} ${classes.p_pad_0}` } style={{ paddingBottom: '12px' }} dangerouslySetInnerHTML={{ __html: body.text_body }} /> }
         </div>
 
         <div className={ classes.cta } style={{ background: fmt_color }}>
