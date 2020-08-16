407549ac554d701428bba01a847d947f650e5d15
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.com>
2020-08-15 23:29:10 -0500

    components/layouts/layout.js, separate Seo from Layout

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -1,48 +1,27 @@
 import Head from 'next/head';
-import { Header, Footer } from './index';
+import { Header, Footer, Seo, GAnalytics, logPageView } from './index';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import classes from '../../styles/classes.module.scss';
 
 Router.onRouteChangeStart = (url) => {
-  console.log(url);
   NProgress.start();
 }
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError    = () => NProgress.done();
 
 export const Layout = (props) => {
-  const { children, single } = props;
-
-  // console.log(props);
-
-  // const { seo_metadata } = single.acf;
-  // const { desc, title }  = seo_metadata.meta_data;
-  // const { og_type }      = seo_metadata;
+  const { children, single, site } = props;
+  const { name } = site;
+  const meta = single.acf.seo_metadata !== null ? single.acf.seo_metadata : {};
+  const title = meta.title ? meta.title : '';
+  const show = meta.show ? meta.show : 0;
+  const meta_title = ( show && title ) ? title : name;
   return (
     <React.Fragment>
       <Head>
-        <title>Make this title dynamic</title>
-        {/* <title>{ title }</title> */}
-        {/* <link rel="canonical" href={ single.link } /> */}
-
-        {/* <meta name="title"       content={ title } />
-        <meta name="description" content={ desc } />
-        <meta name="robots"      content="noindex, nofollow" />
-
-        <meta property="og:site_name"   content="OffWhite Co." />
-        <meta property="og:url"         content={ single.link } />
-        <meta property="og:type"        content={ og_type } />
-        <meta property="og:title"       content={ title } />
-        <meta property="og:description" content={ desc } />
-        <meta property="og:image"       content={ single.image } />
-
-        <meta name="twitter:card"        content="summary" />
-        <meta name="twitter:site"        content="@offwhitedesign" />
-        <meta name="twitter:title"       content={ title } />
-        <meta name="twitter:description" content={ desc } />
-        <meta name="twitter:image"       content={ single.image } />
-        <meta name="twitter:creator"     content="" /> */}
+        <title>{ meta_title }</title>
+        <Seo { ...props } />
       </Head>
 
       <div className={ classes.wrap_app }>
